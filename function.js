3bd40353fd5b598935a9086e17420080cbdf658c
shaikh <32182592+JetShaikh@users.noreply.github.com>
2017-10-14 13:44:03 -0700

    Create function.js

---
+++ function.js
@@ -0,0 +1,23 @@
+console.log("Hey Y'all!")
+
+// Assign the result of number addition to a variable
+let sum = 5 + 7;
+console.log(sum);  // 12
+
+
+// Perform an operation in a parameter 
+console.log(6 / 2);  // 3
+
+
+// Increment (++) the value of a number variable by 1
+let number = 3;
+number++; 
+console.log(number);  // 4
+
+// Adjust the vale of a number by some amount greater than 1
+number += 60;
+console.log(number);  // 64
+number -= 5;
+console.log(number);  // 59
+number *= 2;
+console.log(number);  // 118
